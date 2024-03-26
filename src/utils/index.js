f389071a1144e26288e6c6a320c19565c1855be0
mego1907 <ahmedabdelmageed18@gmail.com>
2024-03-26 22:45:18 +0200

    fix localStorage 2

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,19 @@
 
 
 export const getItemFromLocalStorage = (key) => {
-  if(typeof global?.window !== undefined) {
+  if(typeof global?.window !== "undefined") {
     return localStorage.getItem(key);
   } 
 }
 
 export const setItemInLocalStorage = (key, value) => {
-  if(typeof global?.window !== undefined) {
+  if(typeof global?.window !== "undefined") {
     localStorage.setItem(key, value)
   }
 }
 
 export const clearLocalStorage = () => {
-  if(typeof global?.window !== undefined) {
+  if(typeof global?.window !== "undefined") {
     localStorage.clear();
   }
 }
